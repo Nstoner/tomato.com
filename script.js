13da61fb32501588162ef6af4272ab78a481f4ff
Nstoner <hsuw850@gmail.com>
2022-12-03 20:27:07 +0630

    update

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+
+  const tabTag = document.querySelector(".tabs");
+  const sliderTab = document.querySelector(".slider-tab");
+  const tabs = ["home", "shop", "about", "review", "contact"];
+  const menuTag = document.getElementsByClassName("tab");
+
+  const handleClick = (event) => {
+    const clickedTabId = event.target.id;
+    const clickedLiTag = document.getElementById(clickedTabId);
+    const clickedLiTagWidth = clickedLiTag.offsetWidth;
+    const clickedLiTagOffsetLeft = clickedLiTag.offsetLeft;
+    sliderTab.style.width =clickedLiTagWidth + "px";
+    sliderTab.style.transform = `translateX(${clickedLiTagOffsetLeft}px)`;
+    
+  }
+
+  for (let i= 0; i < tabs.length; i++) {
+    const liTag = document.createElement("li");
+    liTag.classList.add("tab");
+    liTag.append(tabs[i].toUpperCase());
+    liTag.addEventListener("click", handleClick);
+    liTag.id = i;
+    tabTag.append(liTag);
+    if ( i === 0) {
+      sliderTab.style.width = liTag.offsetWidth + "px";
+    }
+  }
